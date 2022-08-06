a86838f0faeb6b8a2f978dfae8d4623d3a7cf265
SShyamKumar <shyamng48@gmail.com>
2022-08-06 17:02:41 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [name, setName] = useState('');//usestate for storiing values from input
+// variable, function name to update the value when changes
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="my-name">Shyam</h1>
+        <span id="a">shyamng48@gmail.com</span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +23,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <form>
+      <h1>{name}</h1>
+      <input type="text" placeholder="Enter your name" 
+      value={name}
+      onChange = {(e) => setName(e.target.value)}></input>
+      <button>Submit</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+// render: showcasing the output
